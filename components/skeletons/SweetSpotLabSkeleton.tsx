edc485f4b4b05b124f391d4de4bac08170602404
CODEV-CED <ribandon@gmail.com>
2025-09-14 22:15:08 +0200

    refactor(skeleton): unify SweetSpotLab skeleton with minimal SSR/CSR-identical markup

--- components/skeletons/SweetSpotLabSkeleton.tsx
+++ components/skeletons/SweetSpotLabSkeleton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Skeleton } from '@/components/UI/skeleton';
 
 export default function SweetSpotLabSkeleton() {
@@ -7,35 +8,10 @@ export default function SweetSpotLabSkeleton() {
         <Skeleton className="h-6 w-56" />
         <Skeleton className="h-4 w-80" />
       </div>
-
-      {/* Bloc sliders */}
-      <div className="grid gap-4 md:grid-cols-2">
-        {[...Array(4)].map((_, i) => (
-          <div key={i} className="space-y-2 rounded-lg border p-4">
-            <Skeleton className="h-4 w-32" />
-            <Skeleton className="h-2 w-full" />
-          </div>
-        ))}
-      </div>
-
-      {/* Score + eureka */}
-      <div className="flex items-center gap-4">
-        <Skeleton className="h-6 w-64" />
-        <Skeleton className="h-6 w-32" />
-      </div>
-
-      {/* Liste convergences */}
-      <div className="space-y-3">
-        {[...Array(5)].map((_, i) => (
-          <div key={i} className="flex items-start gap-3 rounded-lg border p-3">
-            <Skeleton className="h-10 w-10 rounded-full" />
-            <div className="flex-1 space-y-2">
-              <Skeleton className="h-4 w-1/3" />
-              <Skeleton className="h-3 w-2/3" />
-            </div>
-            <Skeleton className="h-6 w-16 rounded-full" />
-          </div>
-        ))}
+      <Skeleton className="h-40 w-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-5 w-48" />
+        <Skeleton className="h-24 w-full" />
       </div>
     </div>
   );
